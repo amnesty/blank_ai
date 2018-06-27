b54d0248b163a114a634271761b15c4e67c104e5
frodriguez <frodriguez@es.amnesty.org>
2018-06-27 14:25:51 +0200

    Validación de campos

--- api/js/politica.js
+++ api/js/politica.js
@@ -1,73 +1,94 @@
 (function ($) {
-   jQuery(document).ready(function () {
+  jQuery(document).ready(function () {
 
-  $(".webform-submit").prop("type", "button");
+    $(".webform-submit").prop("type", "button");
 
-  $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").blur(function(){
+    $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").blur(function(){
 
-    if (isEmail($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val()) == true)
-    {
-      var params = {"email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
-      "operation" : 'get_member'};
-      $.ajax({
-        data:  params,
-        url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
-        type:  'post',
-        dataType: 'json',
-        beforeSend: function () {
-          //$("#resultado").html("Procesando, espere por favor...");
-        },
-        success:  function (response) {
-          if ((response.id == null) || (response.no_fundraising == 1))
-          {
-            $('.politica_check').prop('checked',false);
-            $('.politica_check').show();
-          }else {
-            $(".politica_check").prop('checked',true);
-            $('.politica_check').hide();
-          }
-
-        }
-      });
-    }
-  });
-
-  $(".webform-submit").click(function() {
-
-    var node_id = $("input[name='form_id']").val().split('webform_client_form_')[1];
-
-    if (($('.messages').length == 1)) {
-
-      var check = $('.politica_check:checkbox:checked');
-      var check_reminder_modal = $('#ai-accion-firma__masinfo_reminder');
-
-      if(!check.is(":checked") && check_reminder_modal.length > 0 && check_reminder_modal.data("shown") != 1 && $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val() != '') { // in case that exist an reminder_modal div
-        $.magnificPopup.open({
-          items: {
-            src: '#test-popup'
-
+      if (isEmail($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val()) == true)
+      {
+        var params = {"email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
+        "operation" : 'get_member'};
+        $.ajax({
+          data:  params,
+          url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
+          type:  'post',
+          dataType: 'json',
+          beforeSend: function () {
+            //$("#resultado").html("Procesando, espere por favor...");
           },
-          removalDelay: 50,
-          callbacks: {
-            open: function() {
-              var popup = this;
-              var input = popup.currItem.inlineElement.find("input");
-              var check = $("#ai-accion-firma__masinfo");
-              input.prop("checked", false);
-              check.prop("checked", false);
-              $('#test-popup').data("shown", 1);
-              //_paq.push(["trackEvent", "popup_check", "mostrado"]);
+          success:  function (response) {
+            if ((response.id == null) || (response.no_fundraising == 1))
+            {
+              $('.politica_check').prop('checked',false);
+              $('.politica_check').show();
+            }else {
+              $(".politica_check").prop('checked',true);
+              $('.politica_check').hide();
+            }
 
-              input.change(function(){
+          }
+        });
+      }
+    });
+
+    $(".webform-submit").click(function() {
+
+      var node_id = $("input[name='form_id']").val().split('webform_client_form_')[1];
+
+      if (validarForm()){
+
+        var check = $('.politica_check:checkbox:checked');
+        var check_reminder_modal = $('#ai-accion-firma__masinfo_reminder');
+
+        if(!check.is(":checked") && check_reminder_modal.length > 0 && check_reminder_modal.data("shown") != 1 && $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val() != '') { // in case that exist an reminder_modal div
+          $.magnificPopup.open({
+            items: {
+              src: '#test-popup'
+
+            },
+            removalDelay: 50,
+            callbacks: {
+              open: function() {
+                var popup = this;
+                var input = popup.currItem.inlineElement.find("input");
                 var check = $("#ai-accion-firma__masinfo");
-                check.prop("checked", true);
-                $(".politica_check").prop("checked", true);
-                var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-                              "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-                              "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
-                              "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
-                              "politica" : '0',
-                              "operation" : 'insert_member'};
+                input.prop("checked", false);
+                check.prop("checked", false);
+                $('#test-popup').data("shown", 1);
+                //_paq.push(["trackEvent", "popup_check", "mostrado"]);
+
+                input.change(function(){
+                  var check = $("#ai-accion-firma__masinfo");
+                  check.prop("checked", true);
+                  $(".politica_check").prop("checked", true);
+                  var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-first-name").val(),
+                  "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
+                  "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
+                  "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
+                  "politica" : '0',
+                  "operation" : 'insert_member'};
+                  $.ajax({
+                    data:  params,
+                    url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
+                    type:  'post',
+                    dataType: 'json',
+                    beforeSend: function () {
+                      //$("#resultado").html("Procesando, espere por favor...");
+                    },
+                    success:  function (response) {
+                      $("#webform-client-form-"+node_id).submit();
+                    }
+                  });
+                });
+              },
+              beforeClose: function(){
+                var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-first-name").val(),
+                "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
+                "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
+                "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
+                "politica" : '1',
+                "operation" : 'insert_member'};
                 $.ajax({
                   data:  params,
                   url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
@@ -80,65 +101,70 @@
                     $("#webform-client-form-"+node_id).submit();
                   }
                 });
-              });
+              }
             },
-            beforeClose: function(){
-              var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-                            "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-                            "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
-                            "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
-                            "politica" : '1',
-                            "operation" : 'insert_member'};
-              $.ajax({
-                data:  params,
-                url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
-                type:  'post',
-                dataType: 'json',
-                beforeSend: function () {
-                  //$("#resultado").html("Procesando, espere por favor...");
-                },
-                success:  function (response) {
-                  $("#webform-client-form-"+node_id).submit();
-                }
-              });
-            }
-          },
-          midClick: true
-        });
-        event.stopImmediatePropagation();
-        return false;
-      }else if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val() != ''){
+            midClick: true
+          });
+          event.stopImmediatePropagation();
+          return false;
+        }else if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val() != ''){
 
-	      	var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-        	              "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
-                	      "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
-                      	      "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
-	                      "politica" : $('.politica_check:checkbox:checked').is(":checked"),
-        	              "operation" : 'insert_member'};
+          var params = {"nombre" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-first-name").val(),
+          "apellidos" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val(),
+          "telefono" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val(),
+          "email" : $("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val(),
+          "politica" : $('.politica_check:checkbox:checked').is(":checked"),
+          "operation" : 'insert_member'};
 
-        	$.ajax({
-     	 		data:  params,
-	                url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
-        	 	type:  'post',
-                	dataType: 'json',
-	         	beforeSend: function () {},
-		        success:  function (response) {
-        		       $("#webform-client-form-"+node_id).submit();
-         		}});
-	}
-      
-	$("#webform-client-form-"+node_id).submit();
-      	return true;
-    }else {
-        $("#webform-client-form-"+node_id).submit();
-        return true;
+          $.ajax({
+            data:  params,
+            url:   '/sites/all/themes/blank_ai/api/includes/get_member.php',
+            type:  'post',
+            dataType: 'json',
+            beforeSend: function () {},
+            success:  function (response) {
+              $("#webform-client-form-"+node_id).submit();
+            }});
+          }
+
+          $("#webform-client-form-"+node_id).submit();
+          return true;
+        }else{
+          $("#webform-client-form-"+node_id).submit();
+          return true;
+        }
+      }); // on click
+
+    });//.document ready
+  })(jQuery)
+
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+
+  function validarForm() {
+
+    error = 0;
+    if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-first-name").val() == '') {
+      error = 1;
     }
-  }); // on click
 
-});//.document ready
-})(jQuery)
+    if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name").val() == '') {
+      error = 1;
+    }
 
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
+    if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-phone-phone").val() == '') {
+      error = 1;
+    }
+
+    if ($("#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email").val() == '') {
+      error = 1;
+    }
+
+    if(error == 1){
+      return false;
+    }else{
+      return true;
+    }
+  }
