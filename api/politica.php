3c3ed471ddc52db74cba3bbf3ab44e9f1170f0b7
frodriguez <frodriguez@es.amnesty.org>
2018-06-29 08:55:56 +0200

    Operativa para identificar formularios con el check de política

--- api/politica.php
+++ api/politica.php
@@ -1,41 +1,44 @@
 <?php
 
-global $base_url;
-$theme_path = $base_url . "/sites/all/themes/blank_ai/api/";
+if (getComponentPolitica($page['content'][system_main][nodes][$node->nid][webform]['#node']->webform[components], 'politica_check')){
+	global $base_url;
+	$theme_path = $base_url . "/sites/all/themes/blank_ai/api/";
 
-?>
-		<link rel="stylesheet" href="<?php print $theme_path; ?>/js/magnific-popup/dist/magnific-popup.css">
-		<!--<script type="text/javascript" src="<?php print $theme_path; ?>/js/jquery-2.2.0.min.js"></script>-->
-	  <script type="text/javascript" src="<?php print $theme_path; ?>/js/magnific-popup/dist/jquery.magnific-popup.js"></script>
-		<script type="text/javascript" src="<?php print $theme_path; ?>/js/politica.js"></script>
+	?>
+	<link rel="stylesheet" href="<?php print $theme_path; ?>/js/magnific-popup/dist/magnific-popup.css">
+	<script type="text/javascript" src="<?php print $theme_path; ?>/js/magnific-popup/dist/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="<?php print $theme_path; ?>/js/politica.js"></script>
 
 
-		<div id="test-popup" class="modal-dialog mfp-hide white-popup mfp-hide" role="document">
-			<div class="modal-content">
-				<div class="modal-body">
-					<p>Si quieres recibir otras acciones como esta e información adicional de Amnistía Internacional, recuerda marcar la casilla verde antes de enviar tu firma</p>
-					</div>
-					<div class="modal-footer">
-					<!--<form class="ai-accion-firma__form" >-->
-					  <label class="ai-accion-firma__check bigger">
-					      <input type="checkbox" id="ai-accion-firma__masinfo_reminder" class="form-check-input"/>
-					      Quiero recibir acciones para cambiar el mundo
-					  </label>
-					<!--</form>-->
-				</div>
+	<div id="test-popup" class="modal-dialog mfp-hide white-popup mfp-hide" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<p>Si quieres recibir otras acciones como esta e información adicional de Amnistía Internacional, recuerda marcar la casilla verde antes de enviar tu firma</p>
+			</div>
+			<div class="modal-footer">
+				<!--<form class="ai-accion-firma__form" >-->
+				<label class="ai-accion-firma__check bigger">
+					<input type="checkbox" id="ai-accion-firma__masinfo_reminder" class="form-check-input"/>
+					Quiero recibir acciones para cambiar el mundo
+				</label>
+				<!--</form>-->
 			</div>
 		</div>
+	</div>
 
 
-<?php
-  /*include_once('config.php');
-  include_once('connect.php');
-  include_once('api.php');
+	<?php
+}
+//Función que busca un componenete del formulario dentro del array que contiente todos los componentes.
+function getComponentPolitica($arrayComponentes, $TipoComponente)
+{
 
-  $members = get_member_by_email($_POST['email'])[0];
-
-  $return = $members;
-
-  $return["json"] = json_encode($return);
-  echo json_encode($return);*/
+	foreach ($arrayComponentes as $aComponents)
+	{
+		if ($aComponents[form_key][extra][css_classes] == $TipoComponente){
+			return true;
+		}
+	}
+	return false;
+}
 ?>
