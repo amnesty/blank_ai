6ade579709a3b3854deca4f5e5cdae354c1c01af
Helena <hgarcia@es.amnesty.org>
2020-03-06 11:41:42 +0100

    Cambiado orden de elementos en login

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -25,18 +25,19 @@
 
             </div>
 
-            <div class="user-login-links">
-                <p>Si es la primera vez que accedes al área privada, <a href="<?php print base_path();?>area-privada/registro" target="popup"
-                  onclick="window.open('<?php print base_path();?>area-privada/registro','popup','width=600,height=400');return false;">pulsa aquí</a> .</p>
-                <p><span class="password-link"><a href="<?php print base_path();?>user/password">¿Has olvidado tu contraseña?</a></span></p>
-            </div>
-
             <?php
                 // render login button
                 print drupal_render($form['form_build_id']);
                 print drupal_render($form['form_id']);
                 print drupal_render($form['actions']);
             ?>
+
+            <div class="user-login-links">
+              <p><span class="password-link"><a href="<?php print base_path();?>user/password">¿Has olvidado tu contraseña?</a></span></p>
+              <p>Si todavía no tienes acceso al área privada de socias y socios, <a href="<?php print base_path();?>area-privada/registro" target="popup"
+                  onclick="window.open('<?php print base_path();?>area-privada/registro','popup','width=600,height=400');return false;">pulsa aquí</a>.</p>
+            </div>
+            
             <!-- /user-login-custom-form -->
         </div>
 
